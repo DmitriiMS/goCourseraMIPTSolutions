9f56d72b150a2bf7303341a4797c11c727e1a864
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2021-10-16 17:56:27 +0300

    hw1_tree working solution with passing tests, some error handling, and comments

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -1,11 +1,14 @@
+//hw1_tree is the implementation of tree util in go
+//it builds directory tree for given path
+// if -f parameter is provided, it will also print files ant their sizes
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
-	"strings"
+	"sort"
 )
 
 func main() {
@@ -19,4 +22,74 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+//dirTree is an entry point which starts running an actual algorithm
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	err := worker(out, path, printFiles, "")
+	if err != nil { //check for errors
+		return err
+	}
+	return nil
+}
+
+//getSortedDirList creates a list of objects in the given direcory, sorts it and returns.
+//Also, if -f parameter is not provided, it prunes the list, deleting the file objects
+func getSortedDirList(path string, printFiles bool) ([]os.DirEntry, error) {
+	f, err := os.Open(path) //open file and check for errors
+	if err != nil {
+		return nil, errors.New("cant open path")
+	}
+	dirs, _ := f.ReadDir(0) //read contents of a dir and ignore errors because of files
+	f.Close()
+	if !printFiles { //if no -f parameter
+		newDirs := []os.DirEntry{} //temp slice
+		for _, d := range dirs {
+			if d.IsDir() {
+				newDirs = append(newDirs, d) //only if it's a directory, append it to temp
+			}
+		}
+		dirs = newDirs // new slice of only dirs
+	}
+	//simple sort
+	sort.Slice(dirs, func(i, j int) bool { return dirs[i].Name() < dirs[j].Name() })
+	return dirs, nil
+}
+
+//worker utilizes recursion to construct and print each obhect in a tree.
+func worker(out io.Writer, path string, printFiles bool, prefix string) error {
+	dirs, err := getSortedDirList(path, printFiles) //get a list of objects in path
+	if err != nil {                                 //check for errors
+		return err
+	}
+	for i, dir := range dirs {
+		info, err := dir.Info() //get info of an object
+		if err != nil {
+			return err
+		}
+		//init some vars with empty values
+		size := ""
+		lines := ""
+		newPrefix := ""
+		if !dir.IsDir() && printFiles && info.Size() != 0 {
+			size = fmt.Sprintf(" (%db)", info.Size()) // if it's not a dir and -f is present, get the size of a file
+		} else if !dir.IsDir() && printFiles && info.Size() == 0 {
+			size = " (empty)" // if size is 0, print (empty)
+		}
+		switch i { // check if it's the last element in a slice
+		case len(dirs) - 1:
+			lines = "└───" // it it is, make this line and prefix
+			newPrefix = prefix + "	"
+		default:
+			lines = "├───" // if not, use this
+			newPrefix = prefix + "│	"
+		}
+		fmt.Fprintln(out, prefix+lines+dir.Name()+size)               // print the string
+		err = worker(out, path+"/"+dir.Name(), printFiles, newPrefix) // go deeper (if it's a file, it will return and print nothing)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
